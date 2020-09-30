49fdce5f8cb3deb13d3216c75e013f4b96281a34
Charles Edward Thornton <hawthornepresscom@gmail.com>
2020-09-30 15:25:21 -0500

    Add files via upload

---
+++ fib.go
@@ -0,0 +1,28 @@
+/fib.go - Calculate and display first twenty Fibonacci Sequence Values
+//  This program uses a re-entrant process to calculate the Fibonacci Sequence.
+package main
+
+import (
+	"fmt"
+)
+
+//
+// Calculate Fibonacci Number
+//
+func fib(n int) int {
+	if n < 2 {
+		return n
+	}
+	return fib(n-1) + fib(n-2)
+}
+
+//
+// Display first 20 numbers in Fibonacci Sequence
+//
+func main() {
+	for n := 0; n < 20; n++ {
+
+		fmt.Print(fib(n), " ") // Calculate & Display next value in the sequence
+	}
+	fmt.Println("")
+}
